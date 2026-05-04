82e320133f303f0ded51ec49b70f9bc0f6846954
sereneshree <mishu.08nov23@.com>
2026-05-04 19:53:09 +0530

    added comments to twoDarray code

--- twoDarray.java
+++ twoDarray.java
@@ -1,15 +1,15 @@
-import java.util.*;
-public class twoDarray {
-    public static void main(String a[]){
+import java.util.*; //this is to import everything 
+public class twoDarray { //class name
+    public static void main(String a[]){ //
      
-    int nums[][]=new int[3][4];
+    int nums[][]=new int[3][4]; //one 2D array
       
-    for(int i=0;i<3;i++){
-        for(int j=0;j<4;j++){
-            nums[i][j] =(int)(Math.random()*100);
-            System.out.print(nums[i][j]+" ");
+    for(int i=0;i<3;i++){ //outer loop
+        for(int j=0;j<4;j++){ //inner loop
+            nums[i][j] =(int)(Math.random()*100); //needed random numbers in my array
+            System.out.print(nums[i][j]+" "); //to print the output
             }
-        System.out.println();
+        System.out.println(); // to show proper row and columns in output
         }
     }
 }
