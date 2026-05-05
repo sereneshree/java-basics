2f93836a538d04fcee8ca4803ec64787a6955024
sereneshree <mishu.08nov23@.com>
2026-05-05 23:13:02 +0530

    Added JaggedArray code

---
+++ JaggedArray.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class JaggedArray{
+    
+    public static void main(String a[]){
+
+        int nums[][]=new int[3][]; //jagged(means columns can be diff for each row)
+
+        nums[0]=new int[3];
+        nums[1]=new int[4];
+        nums[2]=new int[2];
+
+        for(int i=0;i<nums.length;i++){
+            
+            for(int j=0;j<nums[i].length;j++){
+                nums[i][j]=(int)(Math.random()*100);
+                System.out.print(nums[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
