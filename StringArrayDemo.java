b35483ffc83e315ecb8e6dd8f932ef8a2f237bcd
sereneshree <mishu.08nov23@gmail.com>
2026-05-06 20:12:40 +0530

    Added StringArrayCode

---
+++ StringArrayDemo.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class student{
+    int rollno;
+    String name;
+    int marks;
+}
+public class StringArrayDemo {
+
+    public static void main(String a[]){
+
+        student s1=new student();
+        s1.rollno =1;
+        s1.name= "avinash";
+        s1.marks=88;
+        
+        student s2=new student();
+        s2.rollno =2;
+        s2.name= "bhumi";
+        s2.marks=98;
+        
+        student s3=new student();
+        s3.rollno =3;
+        s3.name= "charu";
+        s3.marks=76;
+
+        student students[]=new student[3];
+        students[0]=s1;
+        students[1]=s2;
+        students[2]=s3;
+
+        for(int i=0;i<students.length;i++){
+            System.out.println(students[i].name + ":" + students[i].marks);  
+        }
+    }   
+}
