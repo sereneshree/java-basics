650c1182db8f0078d7d31ce599b547ac98e5631c
sereneshree <mishu.08nov23@gmail.com>
2026-05-06 20:22:56 +0530

    Added comments to StringArrayCode

--- StringArrayDemo.java
+++ StringArrayDemo.java
@@ -1,13 +1,13 @@
 import java.util.*;
-class student{
+class student{ // Blueprint class to group different data types (int and String) together
     int rollno;
     String name;
     int marks;
 }
-public class StringArrayDemo {
+public class StringArrayDemo { 
 
     public static void main(String a[]){
-
+        // Initialize individual Student objects with specific data
         student s1=new student();
         s1.rollno =1;
         s1.name= "avinash";
@@ -23,13 +23,13 @@ public class StringArrayDemo {
         s3.name= "charu";
         s3.marks=76;
 
-        student students[]=new student[3];
+        student students[]=new student[3];  // an array created to store the values
         students[0]=s1;
         students[1]=s2;
         students[2]=s3;
 
-        for(int i=0;i<students.length;i++){
-            System.out.println(students[i].name + ":" + students[i].marks);  
+        for(int i=0;i<students.length;i++){  // loop
+            System.out.println(students[i].name + ":" + students[i].marks);  //output printing
         }
     }   
 }
