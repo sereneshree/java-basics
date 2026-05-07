580de33fba0e82246791d0a867839b96c96e5cf9
sereneshree <mishu.08nov23@gmail.com>
2026-05-07 19:19:20 +0530

    Added EnhancedForLoop code

---
+++ EnhancedForLoop.java
@@ -0,0 +1,15 @@
+public class EnhancedForLoop { 
+    public static void main(String a[]){
+
+        int nums[]=new int[4];
+        nums[0]=1;
+        nums[1]=3;
+        nums[2]=8;
+        nums[3]=6;   
+
+        for (int n:nums){ //this loop only works for array
+            System.out.println(n);
+        }
+     }
+    
+}
