00e68cfbf3845a3be1fb407d57fc49fd37d64f8e
sereneshree <mishu.08nov23@gmail.com>
2026-05-09 23:13:34 +0530

    Added StringBuffer Code with Methods

--- StrBfrIntl.java
+++ StrBfrIntl.java
@@ -1,9 +1,11 @@
 public class StrBfrIntl {
     public static void main(String a[]){
 
-        StringBuffer sb=new StringBuffer("bhagyashree");
-        System.out.println(sb.capacity());
-        System.out.println(sb.length());
+        StringBuffer sb=new StringBuffer("bhagyashree");  // it will add more space in the character rather than using that 16 character space
+        System.out.println(sb.capacity());  // provides the actual capacity of the string it can hold character of
+        System.out.println(sb.length()); // gives the length of the string provided by the user
+        sb.append(" karn"); // adds value to the previous string rather than making one another new object to store it 
+        sb.deleteCharAt(1); // deletes a character from any specified index
+        System.out.print(sb);
     }
-    
 }
